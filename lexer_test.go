168c8c293f0081767e1f803899ceccdfe232138d
Jeferson Sanches <jsanchesleao@gmail.com>
2022-07-23 09:54:59 +0200

    refactored lexer test

--- lexer_test.go
+++ lexer_test.go
@@ -3,7 +3,6 @@ package grammatic
 import "testing"
 
 func assertTokenEquals(t *testing.T, expected, actual Token) {
-
 	if expected.Name != actual.Name {
 		t.Logf("Expected token with name %q but found %q", expected.Name, actual.Name)
 		t.Fail()
@@ -14,7 +13,16 @@ func assertTokenEquals(t *testing.T, expected, actual Token) {
 		t.Logf("Expected token position to be value %d:%d but found %d:%d", expected.Line, expected.Col, actual.Line, actual.Col)
 		t.Fail()
 	}
+}
 
+func assertTokenList(t *testing.T, expected, actual []Token) {
+	if len(expected) != len(actual) {
+		t.Logf("Should have generated %d tokens, but instead was %d", len(expected), len(actual))
+		t.Fail()
+	}
+	for i, _ := range expected {
+		assertTokenEquals(t, expected[i], actual[i])
+	}
 }
 
 func TestSimpleTokenDef(t *testing.T) {
@@ -34,15 +42,12 @@ func TestSimpleTokenDef(t *testing.T) {
 		t.Log(err)
 		t.Fail()
 	}
-	if len(tokens) != 5 {
-		t.Log("Should have generated 5 tokens, but instead was", len(tokens))
-		t.Logf("%+v\n", tokens)
-		t.Fail()
-	}
-	assertTokenEquals(t, Token{Name: "Keyword", Value: "prop", Col: 1, Line: 1}, tokens[0])
-	assertTokenEquals(t, Token{Name: "Space", Value: " ", Col: 5, Line: 1}, tokens[1])
-	assertTokenEquals(t, Token{Name: "Equals", Value: "=", Col: 6, Line: 1}, tokens[2])
-	assertTokenEquals(t, Token{Name: "Space", Value: " ", Col: 7, Line: 1}, tokens[3])
-	assertTokenEquals(t, Token{Name: "String", Value: "\"value\"", Col: 8, Line: 1}, tokens[4])
+	assertTokenList(t, []Token{
+		{Name: "Keyword", Value: "prop", Col: 1, Line: 1},
+		{Name: "Space", Value: " ", Col: 5, Line: 1},
+		{Name: "Equals", Value: "=", Col: 6, Line: 1},
+		{Name: "Space", Value: " ", Col: 7, Line: 1},
+		{Name: "String", Value: "\"value\"", Col: 8, Line: 1},
+	}, tokens)
 
 }
