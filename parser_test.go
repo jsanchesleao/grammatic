74ca505c1d09e7c9dcace8d38039269d17694535
Jeferson Sanches <jsanchesleao@gmail.com>
2022-07-24 14:02:41 +0200

    testing Or rule combinator

--- parser_test.go
+++ parser_test.go
@@ -1,16 +1,18 @@
 package grammatic
 
 import (
+	"fmt"
 	"testing"
 )
 
-var tokens = []Token{
-	{Type: "TOKEN_KEYWORD", Value: "test", Line: 1, Col: 1},
-	{Type: "TOKEN_EOF", Value: "", Line: 2, Col: 0},
-}
+var int_token = Token{Type: "TOKEN_INT", Value: "1", Line: 1, Col: 1}
+var keyword_token = Token{Type: "TOKEN_KEYWORD", Value: "test", Line: 1, Col: 1}
+var string_token = Token{Type: "TOKEN_STRING", Value: "text", Line: 1, Col: 1}
+var eof_token = Token{Type: "TOKEN_EOF", Value: "1", Line: 1, Col: 1}
 
 func TestSimpleRuleMatch(t *testing.T) {
 	rule := RuleTokenType("TestRule", "TOKEN_KEYWORD")
+	var tokens = []Token{keyword_token, eof_token}
 	match, remaining := rule.Check(tokens)
 
 	if match == nil {
@@ -27,6 +29,7 @@ func TestSimpleRuleMatch(t *testing.T) {
 
 func TestSimpleRuleFail(t *testing.T) {
 	rule := RuleTokenType("TestRule", "TOKEN_STRING")
+	var tokens = []Token{keyword_token, eof_token}
 	match, remaining := rule.Check(tokens)
 
 	if match != nil {
@@ -38,6 +41,7 @@ func TestSimpleRuleFail(t *testing.T) {
 
 func TestSimpleRuleWithValueMatch(t *testing.T) {
 	rule := RuleTokenTypeAndValue("TestRule", "TOKEN_KEYWORD", "test")
+	var tokens = []Token{keyword_token, eof_token}
 	match, remaining := rule.Check(tokens)
 
 	if match == nil {
@@ -54,6 +58,7 @@ func TestSimpleRuleWithValueMatch(t *testing.T) {
 
 func TestSimpleRuleWithValueFail(t *testing.T) {
 	rule := RuleTokenTypeAndValue("TestRule", "TOKEN_KEYWORD", "fail")
+	var tokens = []Token{keyword_token, eof_token}
 
 	match, remaining := rule.Check(tokens)
 
@@ -62,4 +67,54 @@ func TestSimpleRuleWithValueFail(t *testing.T) {
 	}
 
 	AssertTokenList(t, tokens, remaining)
+}
+
+func TestOr(t *testing.T) {
+	rule := Or("KeywordOrIntRule",
+		RuleTokenType("KeywordRule", "TOKEN_KEYWORD"),
+		RuleTokenType("IntRule", "TOKEN_INT"))
+
+	tokensWithInt := []Token{int_token, eof_token}
+	tokensWithKeyword := []Token{keyword_token, eof_token}
+	tokensWithString := []Token{string_token, eof_token}
+
+	intMatch, remainingInt := rule.Check(tokensWithInt)
+	keywordMatch, remainingKeyword := rule.Check(tokensWithKeyword)
+	stringMatch, remainingString := rule.Check(tokensWithString)
+
+	if intMatch == nil || keywordMatch == nil {
+		t.Fatalf("Rule %q should have matched a token in both Int and Keyword types", rule.Type)
+	}
+
+	if stringMatch != nil {
+		t.Fatalf("Rule %q should not have matched a TOKEN_STRING token.\n %+v", rule.Type, stringMatch)
+	}
+
+	fmt.Printf("INT MATCH\n\n%#v\n\n", intMatch)
+
+	AssertRuleMatchEquals(t, RuleMatch{
+		Type: "KeywordOrIntRule",
+		Rules: []RuleMatch{
+			{
+				Type:   "IntRule",
+				Rules:  nil,
+				Tokens: []Token{int_token},
+			},
+		},
+		Tokens: nil,
+	}, *intMatch)
+	AssertRuleMatchEquals(t, RuleMatch{
+		Type: "KeywordOrIntRule",
+		Rules: []RuleMatch{
+			{
+				Type:   "KeywordRule",
+				Rules:  nil,
+				Tokens: []Token{keyword_token},
+			},
+		},
+		Tokens: nil,
+	}, *keywordMatch)
+	AssertTokenList(t, []Token{eof_token}, remainingInt)
+	AssertTokenList(t, []Token{eof_token}, remainingKeyword)
+	AssertTokenList(t, tokensWithString, remainingString)
 }
--- test_utils.go
+++ test_utils.go
@@ -3,17 +3,38 @@ package grammatic
 import "testing"
 
 func AssertTokenEquals(t *testing.T, expected, actual Token) {
+	t.Helper()
 	if expected.Type != actual.Type || expected.Value != actual.Value || expected.Col != actual.Col || expected.Line != actual.Line {
 		t.Fatalf("Expected %+v but found %+v", expected, actual)
 	}
 }
 
 func AssertTokenList(t *testing.T, expected, actual []Token) {
+	t.Helper()
 	if len(expected) != len(actual) {
 		t.Logf("Should have generated %d tokens, but instead was %d", len(expected), len(actual))
 		t.Fail()
 	}
-	for i, _ := range expected {
+	for i := range expected {
 		AssertTokenEquals(t, expected[i], actual[i])
 	}
+}
+
+func AssertRuleMatchEquals(t *testing.T, expected, actual RuleMatch) {
+	t.Helper()
+
+	if expected.Type != actual.Type {
+		t.Fatalf("Expected a matching rule of type %q buf got %q", expected.Type, actual.Type)
+	}
+
+	AssertTokenList(t, expected.Tokens, actual.Tokens)
+
+	if len(expected.Rules) != len(actual.Rules) {
+		t.Fatalf("Expected to found %d matched subrules, but found %d", len(expected.Rules), len(actual.Rules))
+	}
+
+	for i, ruleMatch := range expected.Rules {
+		AssertRuleMatchEquals(t, ruleMatch, actual.Rules[i])
+	}
+
 }
