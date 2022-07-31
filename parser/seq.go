7aae351feaa946fcae177c8d2d38def3c0a1c347
Jeferson Sanches <jsanchesleao@gmail.com>
2022-07-31 23:11:50 +0200

    seq error test done and passing

--- parser/seq.go
+++ parser/seq.go
@@ -1,11 +1,10 @@
 package parser
 
-import "grammatic/model"
+import (
+	"grammatic/model"
+)
 
 func Seq(ruleType string, rules ...*model.Rule) *model.Rule {
-	if len(rules) == 0 {
-		panic("Provide at least one rule to Seq combinator")
-	}
 	return &model.Rule{
 		Type: ruleType,
 		Check: func(tokens []model.Token) model.RuleResultIterator {
@@ -17,8 +16,69 @@ func Seq(ruleType string, rules ...*model.Rule) *model.Rule {
 					return
 				}
 
-				// logic goes here
+				if len(rules) == 0 {
+					stream.Send(&model.RuleResult{
+						Match: &model.Node{
+							Type:  ruleType,
+							Token: nil,
+							Rules: nil,
+						},
+						RemainingTokens: tokens,
+						Error:           nil,
+					})
+					stream.Done()
+					return
+				}
 
+				headRule := rules[0]
+				tailRule := Seq(ruleType, rules[1:]...)
+
+				headIterator := headRule.Check(tokens)
+				var error *model.RuleError = nil
+				for {
+					headResult := headIterator.Next()
+					if headResult == nil {
+						break
+					}
+					if headResult.Error != nil {
+						error = headResult.Error
+						continue
+					}
+					tailIterator := tailRule.Check(headResult.RemainingTokens)
+					tailResult := tailIterator.Next()
+				tail:
+					for {
+						if tailResult == nil {
+							break tail
+						}
+						if tailResult.Error != nil {
+							error = tailResult.Error
+							tailResult = tailIterator.Next()
+							continue tail
+						}
+						stream.Send(&model.RuleResult{
+							Match: &model.Node{
+								Type:  ruleType,
+								Token: nil,
+								Rules: append([]model.Node{*headResult.Match}, tailResult.Match.Rules...),
+							},
+							RemainingTokens: tailResult.RemainingTokens,
+							Error:           nil,
+						})
+						stream.Continue()
+						stream.Done()
+						tailIterator.Done()
+						headIterator.Done()
+						return
+					}
+				}
+				stream.Send(&model.RuleResult{
+					Match:           nil,
+					RemainingTokens: tokens,
+					Error:           error,
+				})
+
+				stream.Continue()
 				stream.Done()
 			}()
 
--- parser/seq_test.go
+++ parser/seq_test.go
@@ -9,7 +9,7 @@ func TestSeq(t *testing.T) {
 	rule := Seq("IntThenString",
 		RuleTokenType("LParen", "TOKEN_LPAREN"),
 		RuleTokenType("Boolean", "TOKEN_BOOL"),
-		RuleTokenType("RParem", "TOKEN_RPAREN"),
+		RuleTokenType("RParen", "TOKEN_RPAREN"),
 	)
 
 	tokens := []model.Token{lparen_token, bool_token, rparen_token, eof_token}
@@ -53,5 +53,38 @@ func TestSeq(t *testing.T) {
 	}, *result.Match)
 
 	model.AssertTokenList(t, []model.Token{eof_token}, result.RemainingTokens)
+}
 
+func TestSeqFail(t *testing.T) {
+	rule := Seq("IntThenString",
+		RuleTokenType("LParen", "TOKEN_LPAREN"),
+		RuleTokenType("Boolean", "TOKEN_BOOL"),
+		RuleTokenType("RParen", "TOKEN_RPAREN"),
+	)
+
+	tokens := []model.Token{lparen_token, bool_token, lparen_token, eof_token}
+
+	iterator := rule.Check(tokens)
+	result := iterator.Next()
+
+	if result == nil {
+		t.Fatalf("Expected first result to be not nil, but it was")
+	}
+
+	nextResult := iterator.Next()
+	if nextResult != nil {
+		t.Fatalf("Expected second candidate to be nil, but was %#v", nextResult)
+	}
+
+	if result.Error == nil {
+		t.Fatalf("Expected error to not be nil, but was")
+	}
+
+	expectedErrorMessage := "Unexpected token \"(\" at line 1, column 1"
+	errorMessage := result.Error.GetError().Error()
+	if errorMessage != expectedErrorMessage {
+		t.Fatalf("Expected error message to be\n%q\n but was\n%q\n", expectedErrorMessage, errorMessage)
+	}
+
+	model.AssertTokenList(t, tokens, result.RemainingTokens)
 }
