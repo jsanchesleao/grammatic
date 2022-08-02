742063a17d3a3382399aa7db2a3e6e574d32af26
Jeferson Sanches <jsanchesleao@gmail.com>
2022-08-02 18:42:23 +0200

    one or none combinator done

--- parser/one_or_none.go
+++ parser/one_or_none.go
@@ -14,6 +14,44 @@ func OneOrNone(ruleType string, rule *model.Rule) *model.Rule {
 					return
 				}
 
+				iterator := rule.Check(tokens)
+
+				for {
+					result := iterator.Next()
+					if result == nil {
+						break
+					}
+
+					if result.Error != nil {
+						continue
+					}
+
+					stream.Send(&model.RuleResult{
+						Match: &model.Node{
+							Type:  ruleType,
+							Token: nil,
+							Rules: []model.Node{*result.Match},
+						},
+						RemainingTokens: result.RemainingTokens,
+						Error:           nil,
+					})
+					if !stream.Continue() {
+						iterator.Done()
+						stream.Done()
+						return
+					}
+				}
+
+				stream.Send(&model.RuleResult{
+					Match: &model.Node{
+						Type:  ruleType,
+						Token: nil,
+						Rules: []model.Node{},
+					},
+					RemainingTokens: tokens,
+					Error:           nil,
+				})
+				stream.Continue()
 				stream.Done()
 			}()
 
