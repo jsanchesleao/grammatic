311e0c2f56ef0aee2c97ef6b66a5a960e4faadec
Jeferson Sanches <dasilvasanchesle@mediamarktsaturn.com>
2022-08-03 09:59:52 +0200

    grammar object api improvements

--- grammar.go
+++ grammar.go
@@ -12,6 +12,10 @@ type Grammar struct {
 	IgnoredTokenTypes []string
 }
 
+type GrammarCombinator struct {
+	Create func(string) *model.Rule
+}
+
 func NewGrammar() Grammar {
 	return Grammar{
 		Rules:             map[string]*model.Rule{},
@@ -31,17 +35,45 @@ func (g *Grammar) GetRule(name string) *model.Rule {
 	return g.Rules[name]
 }
 
-func (g *Grammar) DefineRule(rule *model.Rule) {
-	g.DeclareRule(rule.Type)
-	if g.Rules[rule.Type].Type != rule.Type {
+func (g *Grammar) DefineRule(ruleType string, combinator GrammarCombinator) {
+	g.DeclareRule(ruleType)
+	if g.Rules[ruleType].Type != ruleType {
 		panic("Cannot override rule type")
 	}
-	*g.Rules[rule.Type] = *rule
+	*g.Rules[ruleType] = *combinator.Create(ruleType)
+}
+
+func (g *Grammar) Or(ruleNames ...string) GrammarCombinator {
+	rules := []*model.Rule{}
+	for _, name := range ruleNames {
+		rules = append(rules, g.GetRule(name))
+	}
+	return GrammarCombinator{
+		Create: func(ruleType string) *model.Rule {
+			return parser.Or(ruleType, rules...)
+		},
+	}
+}
+
+func (g *Grammar) Seq(ruleNames ...string) GrammarCombinator {
+	rules := []*model.Rule{}
+	for _, name := range ruleNames {
+		rules = append(rules, g.GetRule(name))
+	}
+	return GrammarCombinator{
+		Create: func(ruleType string) *model.Rule {
+			return parser.Seq(ruleType, rules...)
+		},
+	}
 }
 
 func (g *Grammar) DefineToken(name, pattern string) {
 	g.TokenDefs = append(g.TokenDefs, lexer.NewTokenDef(name, pattern))
-	g.DefineRule(parser.RuleTokenType(name, name))
+	g.DefineRule(name, GrammarCombinator{
+		Create: func(name string) *model.Rule {
+			return parser.RuleTokenType(name, name)
+		},
+	})
 }
 
 func (g *Grammar) DefineIgnoredToken(name, pattern string) {
--- grammar_test.go
+++ grammar_test.go
@@ -3,7 +3,6 @@ package grammatic
 import (
 	"fmt"
 	"grammatic/lexer"
-	"grammatic/parser"
 	"testing"
 )
 
@@ -11,34 +10,11 @@ func TestGrammar(t *testing.T) {
 
 	g := NewGrammar()
 
-	g.DefineRule(parser.Or("Expression",
-		g.GetRule("Term"),
-		g.GetRule("PlusExpr"),
-		g.GetRule("MinusExpr"),
-	))
-
-	g.DefineRule(parser.Seq("PlusExpr",
-		g.GetRule("Expression"),
-		g.GetRule("Plus"),
-		g.GetRule("Expression"),
-	))
-
-	g.DefineRule(parser.Seq("MinusExpr",
-		g.GetRule("Expression"),
-		g.GetRule("Minus"),
-		g.GetRule("Expression"),
-	))
-
-	g.DefineRule(parser.Or("Term",
-		g.GetRule("Number"),
-		g.GetRule("ParensExpr"),
-	))
-
-	g.DefineRule(parser.Seq("ParensExpr",
-		g.GetRule("LeftParens"),
-		g.GetRule("Expression"),
-		g.GetRule("RightParens"),
-	))
+	g.DefineRule("Expression", g.Or("Term", "PlusExpr", "MinusExpr"))
+	g.DefineRule("PlusExpr", g.Seq("Expression", "Plus", "Expression"))
+	g.DefineRule("MinusExpr", g.Seq("Expression", "Minus", "Expression"))
+	g.DefineRule("Term", g.Or("Number", "ParensExpr"))
+	g.DefineRule("ParensExpr", g.Seq("LeftParens", "Expression", "RightParens"))
 
 	g.DefineToken("Number", lexer.NumberTokenFormat)
 	g.DefineToken("LeftParens", "^\\(")
