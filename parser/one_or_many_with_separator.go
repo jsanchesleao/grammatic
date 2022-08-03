d46ee0a7ed642a33db6b91dd448983fab9fc8ffc
Jeferson Sanches <jsanchesleao@gmail.com>
2022-08-03 17:54:27 +0200

    one or many with separator

---
+++ parser/one_or_many_with_separator.go
@@ -0,0 +1,104 @@
+package parser
+
+import (
+	"fmt"
+	"grammatic/model"
+)
+
+func OneOrManyWithSeparator(typeName string, rule *model.Rule, separator *model.Rule) *model.Rule {
+	return &model.Rule{
+		Type: typeName,
+		Check: func(tokens []model.Token) model.RuleResultIterator {
+
+			stream := NewResultStream()
+
+			go func() {
+
+				if !stream.Continue() {
+					stream.Done()
+					return
+				}
+
+				success := false
+				var error model.RuleError
+				iterator := rule.Check(tokens)
+				subrule := Many(fmt.Sprintf("%s:Tail", typeName), Seq(fmt.Sprintf("%s:TailItem", typeName), separator, rule))
+
+				for {
+					result := iterator.Next()
+
+					if result == nil {
+						iterator.Done()
+						break
+					}
+
+					if result.Error != nil {
+						if result.Error.Token.IsAfter(error.Token) {
+							error = *result.Error
+						}
+						continue
+					}
+
+					tailIterator := subrule.Check(result.RemainingTokens)
+				inner:
+					for {
+						tailResult := tailIterator.Next()
+
+						if tailResult == nil {
+							tailIterator.Done()
+							break inner
+						}
+
+						if tailResult.Error != nil {
+							continue
+						}
+
+						seqNodes := tailResult.Match.GetNodesWithType(fmt.Sprintf("%s:TailItem", typeName))
+						nodes := []model.Node{}
+
+						if result.Match != nil {
+							nodes = append(nodes, *result.Match)
+						}
+
+						for _, node := range seqNodes {
+							nodes = append(nodes, node.Rules...)
+						}
+
+						match := model.Node{
+							Type:  typeName,
+							Token: nil,
+							Rules: nodes,
+						}
+
+						success = true
+						stream.Send(&model.RuleResult{
+							Match:           &match,
+							RemainingTokens: tailResult.RemainingTokens,
+							Error:           nil,
+						})
+						if !stream.Continue() {
+							iterator.Done()
+							tailIterator.Done()
+							return
+						}
+					}
+				}
+
+				if !success {
+					stream.Send(&model.RuleResult{
+						Match:           nil,
+						RemainingTokens: tokens,
+						Error:           &error,
+					})
+					stream.Continue()
+				}
+
+				stream.Done()
+
+			}()
+
+			return stream
+
+		},
+	}
+}
---
+++ parser/one_or_many_with_separator_test.go
@@ -0,0 +1,126 @@
+package parser
+
+import (
+	"grammatic/model"
+	"testing"
+)
+
+func TestOneManyWithSeparatorSuccess(t *testing.T) {
+	rule := OneOrManyWithSeparator("IntByCommas",
+		RuleTokenType("IntRule", "TOKEN_INT"),
+		RuleTokenType("Comma", "TOKEN_COMMA"),
+	)
+	tokens := []model.Token{int_token, comma_token, int_token, eof_token}
+
+	iterator := rule.Check(tokens)
+
+	results := []*model.RuleResult{
+		iterator.Next(),
+		iterator.Next(),
+		iterator.Next(),
+	}
+
+	// Checking for nil
+	if results[0] == nil {
+		t.Fatalf("Expected first result to be non nil, but it was nil")
+	}
+	if results[1] == nil {
+		t.Fatalf("Expected second result to be non nil, but it was nil")
+	}
+	if results[2] != nil {
+		t.Fatalf("Expected third result to be nil, but it was %+v", results[2])
+	}
+
+	// Errors
+	if results[0].Error != nil {
+		t.Fatalf("Expected first result to not have an error, but it had %+v", results[0].Error)
+	}
+	if results[1].Error != nil {
+		t.Fatalf("Expected second result to not have an error, but it had %+v", results[0].Error)
+	}
+
+	// RemainingTokens
+	model.AssertTokenList(t,
+		[]model.Token{eof_token},
+		results[0].RemainingTokens,
+	)
+	model.AssertTokenList(t,
+		[]model.Token{comma_token, int_token, eof_token},
+		results[1].RemainingTokens,
+	)
+
+	model.AssertNodeEquals(t,
+		model.Node{
+			Type:  "IntByCommas",
+			Token: nil,
+			Rules: []model.Node{
+				{
+					Type:  "IntRule",
+					Token: &int_token,
+					Rules: nil,
+				},
+				{
+					Type:  "Comma",
+					Token: &comma_token,
+					Rules: nil,
+				},
+				{
+					Type:  "IntRule",
+					Token: &int_token,
+					Rules: nil,
+				},
+			},
+		},
+		*results[0].Match,
+	)
+	model.AssertNodeEquals(t,
+		model.Node{
+			Type:  "IntByCommas",
+			Token: nil,
+			Rules: []model.Node{
+				{
+					Type:  "IntRule",
+					Token: &int_token,
+					Rules: nil,
+				},
+			},
+		},
+		*results[1].Match,
+	)
+}
+
+func TestOneOrManyWithSeparatorFail(t *testing.T) {
+	rule := OneOrManyWithSeparator("IntByCommas",
+		RuleTokenType("IntRule", "TOKEN_INT"),
+		RuleTokenType("Comma", "TOKEN_COMMA"),
+	)
+	tokens := []model.Token{comma_token, int_token, comma_token, eof_token}
+
+	iterator := rule.Check(tokens)
+
+	results := []*model.RuleResult{
+		iterator.Next(),
+		iterator.Next(),
+	}
+
+	if results[0] == nil {
+		t.Fatalf("Expected first result to be non nil, but it was nil")
+	}
+	if results[1] != nil {
+		t.Fatalf("Expected second result to be nil, but it was %+v", results[1])
+	}
+
+	if results[0].Error == nil {
+		t.Fatalf("Expected error to exist, but it was nil")
+	}
+
+	expectedError := "Unexpected token \",\" at line 1, column 3"
+	actualError := results[0].Error.GetError().Error()
+
+	if expectedError != actualError {
+		t.Fatalf("Expected error message to be %q but it was %q", expectedError, actualError)
+	}
+
+	model.AssertTokenList(t, tokens, results[0].RemainingTokens)
+
+}
